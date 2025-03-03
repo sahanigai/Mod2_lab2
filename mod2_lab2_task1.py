9a3f99e9f8e623ea451c91e8684d25826d8eab78
sahanigai <sahanigai2017@gmail.com>
2025-03-03 23:15:55 +0300

    Update mod2_lab2_task1.py

--- mod2_lab2_task1.py
+++ mod2_lab2_task1.py
@@ -10,24 +10,28 @@ BOOKS_DATABASE = [
         "pages": 400,
     }
 ]
+
+
+# TODO написать класс Book
 class Book:
-    def __init__(self, id_, name, pages):
+    def init(self, id_, name, pages):
         self.id = id_
         self.name = name
         self.pages = pages
 
-    def __str__(self):
+    def str(self):
         return f'Книга "{self.name}"'
 
-    def __repr__(self):
-        return f'Book(id_={self.id}, name={repr(self.name)}, pages={self.pages})'
+    def repr(self):
+        return f"Book(id_={self.id}, name='{self.name}', pages={self.pages})"
 
-if __name__ == '__main__':
+
+if name == 'main':
     # инициализируем список книг
     list_books = [
         Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
     ]
     for book in list_books:
-        print(book)  # проверяем метод __str__
+        print(book)  # проверяем метод str
 
-    print(list_books)  # проверяем метод __repr__
+    print(list_books)  # проверяем метод repr
